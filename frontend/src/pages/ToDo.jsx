30225c5de41516f9c3d0741844820698537f4169
Gerard Garcia <garcia.gerard10@outlook.com>
2022-11-05 19:56:17 +1300

    added to - do list

--- frontend/src/pages/ToDo.jsx
+++ frontend/src/pages/ToDo.jsx
@@ -1,37 +1,119 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import React, { useState } from "react";
+import "./ToDo.css";
 import { Link } from "react-router-dom";
-import Timer from "../components/Timer";
+import { BsArrowLeftSquareFill } from "react-icons/bs";
 
-function Quiz2 () {
-    const { user } = useSelector((state) => state.auth);
-    const navigate = useNavigate();
+function App () {
+    // State Hook - `useState`
+    const [newItem, setNewItem] = useState("");
+    const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        if (!user) {
-            navigate("/login");
+    const [showEdit, setShowEdit] = useState(-1);
+    const [updatedText, setUpdatedText] = useState("");
+
+    /* Adds a new item to the list array*/
+    function addItem () {
+        // ! Check for empty item
+        if (!newItem) {
+            alert("Press enter an item.");
+            return;
         }
-    }, [user, navigate]);
 
+        const item = {
+            id: Math.floor(Math.random() * 1000),
+            value: newItem,
+        };
+
+        // Add new item to items array
+        setItems((oldList) => [...oldList, item]);
+
+        // Reset newItem back to original state
+        setNewItem("");
+    }
+
+    /* Deletes an item based on the `item.id` key */
+    function deleteItem (id) {
+        const newArray = items.filter((item) => item.id !== id);
+        setItems(newArray);
+    }
+
+    /* Edit an item text after creating it. */
+    function editItem (id, newText) {
+        // Get the current item
+        const currentItem = items.filter((item) => item.id === id);
+
+        // Create a new item with same id
+        const newItem = {
+            id: currentItem.id,
+            value: newText,
+        };
+
+        deleteItem(id);
+
+        // Replace item in the item list
+        setItems((oldList) => [...oldList, newItem]);
+        setUpdatedText("");
+        setShowEdit(-1);
+    }
+
+    // Main part of app
     return (
-        <>
-            <div className="page-content">
-                <h1> Tahi - Python Basics</h1>
-                <h1> Quiz</h1>
-                <Timer />
-                <div className="content-box">
-                    <div className="heading">
-                        <p>Content coming soon, watch this space...</p>
-                    </div>
-                </div>
-                <Link to="/modules">
-                    <button className="btn btn-big">Menu</button>
-                </Link>
-            </div>
-        </>
+        <div className="app">
+            {/* 1. Header  */}
+            <h1>My Todo List</h1>
+            <h3> Click on an item to edit </h3>
+            {/* 2. Add new item (input) */}
+            <input
+                type="text"
+                placeholder="Add an item..."
+                value={newItem}
+                onChange={(e) => setNewItem(e.target.value)}
+            />
+
+            {/* Add (button) */}
+            <button onClick={() => addItem()}>Add</button>
+
+            {/* 3. List of todos (unordered list) */}
+            <ul>
+                {items.map((item) => {
+                    return (
+                        <div>
+                            <li key={item.id} onClick={() => setShowEdit(item.id)}>
+                                {item.value}
+                                <button
+                                    className="delete-button"
+                                    onClick={() => deleteItem(item.id)}
+                                >
+                                    ❌
+                                </button>
+                            </li>
+
+                            {showEdit == item.id ? (
+                                <div>
+                                    <input
+                                        type="text"
+                                        value={updatedText}
+                                        onChange={(e) => setUpdatedText(e.target.value)}
+                                    />
+                                    <button onClick={() => editItem(item.id, updatedText)}>
+                                        Update
+                                    </button>
+                                </div>
+                            ) : null}
+                        </div>
+                    );
+                })}
+            </ul>
+            <br />
+            <Link to="/modules">
+                <button className="btn btn-big">
+                    <BsArrowLeftSquareFill />
+                    Menu
+                </button>
+            </Link>
+        </div>
+
     );
 }
 
-export default Quiz2;
+export default App;
